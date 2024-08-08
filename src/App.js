b9ba949d74ca28af3ca10a33e1d916465ff2d0e4
code-iddih <idinyamwaya@gmail.com>
2024-08-08 15:21:11 +0300

    Adds Parent Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import NavBar from "./components/NavBar";
 
-function App() {
-  return (
-    <>
-      <header>
-        
-      </header>
-    </>
-  );
+function App(){
+    return(
+        <>
+            <header>
+                <NavBar />
+            </header>
+        </>
+    );
 };
 
 export default App;
